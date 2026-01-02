3dfaf6edce0398400f065b9562059bf2db905ad9
Jabed Hossain <jhshifat21@gmail.com>
2026-01-02 12:40:44 +0000

    File changes

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import { useToast } from "@/hooks/use-toast"
@@ -29,7 +30,7 @@ export function Toaster() {
           </Toast>
         )
       })}
-      <ToastViewport />
+      <ToastViewport className="sm:top-0 sm:right-0 sm:flex-col" />
     </ToastProvider>
   )
 }
