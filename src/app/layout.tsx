36459a816baf06caffcdca5bf882d40de983a673
Jabed Hossain <jhshifat21@gmail.com>
2026-01-02 18:00:20 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,6 +59,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
+        suppressHydrationWarning={true}
         className={cn(
           'min-h-screen bg-background font-sans antialiased',
           inter.variable,
