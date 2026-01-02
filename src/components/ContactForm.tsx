c3d532491cfaf0d4329e35008735c3c1faaf4f5f
Jabed Hossain <jhshifat21@gmail.com>
2026-01-02 02:23:52 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -2,7 +2,7 @@
 
 import { useFormState, useFormStatus } from 'react-dom';
 import { useEffect, useRef } from 'react';
-import { submitContactForm from '@/lib/actions';
+import { submitContactForm } from '@/lib/actions';
 import { Card, CardContent, CardFooter } from '@/components/ui/card';
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
